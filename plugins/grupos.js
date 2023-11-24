a22ac4faafde7bff8b1094bc464b505d312ac68c
nuevo24 <141083258+nuevo24@users.noreply.github.com>
2023-11-24 18:21:31 -0500

    Update grupos.js

--- plugins/grupos.js
+++ plugins/grupos.js
@@ -18,7 +18,7 @@ let rtotalreg = Object.values(global.db.data.users).filter(user => user.register
 let more = String.fromCharCode(8206)
 let readMore = more.repeat(850)   
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
-let str = `╭「➻𝐋𝐚𝐯𝐢𝐞𝐣𝐚𝐬𝐜𝐨𝐨𝐥 𝟐𝟒𝐤」
+let str = `╭「➻TEAM WOOT」
 │➯𝙷𝚘𝚕𝚊,➟${taguser}
 │➯𝙼𝙸 𝙲𝚁𝙴𝙰𝙳𝙾𝚁:Aleizn
 │➯*Número*➟51992621601
