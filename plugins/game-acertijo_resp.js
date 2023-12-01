33dd9e9e18ccfbf0ce907a2aa0df67444999b1af
nuevo24 <141083258+nuevo24@users.noreply.github.com>
2023-12-01 03:27:34 -0500

    Update game-acertijo_resp.js

--- plugins/game-acertijo_resp.js
+++ plugins/game-acertijo_resp.js
@@ -5,12 +5,12 @@ handler.before = async function (m) {
 let id = m.chat
 if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/^ⷮ/i.test(m.quoted.text)) return !0
 this.tekateki = this.tekateki ? this.tekateki : {}
-if (!(id in this.tekateki)) return m.reply('MIRA BIEN PES ENSALADA! EL ACERTIJO ya ha terminado!')
+if (!(id in this.tekateki)) return m.reply('EL ACERTIJO YA TERMINÓ OE')
 if (m.quoted.id == this.tekateki[id][0].id) {
 let json = JSON.parse(JSON.stringify(this.tekateki[id][1]))
 if (m.text.toLowerCase() == json.response.toLowerCase().trim()) {
 global.db.data.users[m.sender].exp += this.tekateki[id][2]
-m.reply(`*RESPUES CORRECTA*\n+${this.tekateki[id][2]} Exp`)
+m.reply(`*RESPUESTA CORRECTA*\n+${this.tekateki[id][2]} Exp`)
 clearTimeout(this.tekateki[id][3])
 delete this.tekateki[id]
 } else if (similarity(m.text.toLowerCase(), json.response.toLowerCase().trim()) >= threshold) m.reply(`Casi lo logras!`)
