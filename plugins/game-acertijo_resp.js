22db94fb1d87b8ef26716b5cfa4b42261ce90cef
nuevo24 <141083258+nuevo24@users.noreply.github.com>
2023-12-01 03:28:47 -0500

    Update game-acertijo_resp.js

--- plugins/game-acertijo_resp.js
+++ plugins/game-acertijo_resp.js
@@ -10,7 +10,7 @@ if (m.quoted.id == this.tekateki[id][0].id) {
 let json = JSON.parse(JSON.stringify(this.tekateki[id][1]))
 if (m.text.toLowerCase() == json.response.toLowerCase().trim()) {
 global.db.data.users[m.sender].exp += this.tekateki[id][2]
-m.reply(`*RESPUESTA CORRECTA*\n+${this.tekateki[id][2]} Exp`)
+m.reply(`*UY YA,RESPUESTA CORRECTA*\n+${this.tekateki[id][2]} Exp`)
 clearTimeout(this.tekateki[id][3])
 delete this.tekateki[id]
 } else if (similarity(m.text.toLowerCase(), json.response.toLowerCase().trim()) >= threshold) m.reply(`Casi lo logras!`)
